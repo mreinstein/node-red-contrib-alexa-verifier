6d14582209de24aef88624ed0024abb175bce4e3
Mike Reinstein <reinstein.mike@gmail.com>
2017-01-30 09:28:13 -0800

    style tweaking

--- alexa-verifier/alexa-verifier.js
+++ alexa-verifier/alexa-verifier.js
@@ -1,32 +1,32 @@
-var verifier = require('alexa-verifier');
+var verifier = require('alexa-verifier')
 
 
 module.exports = function(RED) {
   function alexaVerifierNode(config) {
-    RED.nodes.createNode(this, config);
-    var node = this;
+    RED.nodes.createNode(this, config)
+    var node = this
     this.on('input', function(msg) {
-      var cert_url  = msg.req.headers.signaturecertchainurl;
-      var signature = msg.req.headers.signature;
-      var body = msg.req.body;
+      var cert_url  = msg.req.headers.signaturecertchainurl
+      var signature = msg.req.headers.signature
+      var body = msg.req.body
       if (typeof body === 'object') {
         try {
-          body = JSON.stringify(body);
+          body = JSON.stringify(body)
         } catch (er) {
-          node.error('failed to parse alexa request body:' + er);
-          return;
+          node.error('failed to parse alexa request body:' + er)
+          return
         }
       }
       verifier(cert_url, signature, body, function(er) {
         if (er) {
-          node.error('error validating the alexa cert:' + er);
-          msg.res.status(401).json({ status: 'failure', reason: er });
+          node.error('error validating the alexa cert:' + er)
+          msg.res.status(401).json({ status: 'failure', reason: er })
         } else {
-          node.log('successfully validated alexa');
-          node.send(msg);
+          node.log('successfully validated alexa')
+          node.send(msg)
         }
-      });
-    });
+      })
+    })
   }
-  RED.nodes.registerType('alexa-verifier', alexaVerifierNode);
-};
+  RED.nodes.registerType('alexa-verifier', alexaVerifierNode)
+}
